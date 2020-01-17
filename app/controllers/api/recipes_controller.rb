aa2b7579b665faa93cdf39d829581c50b23f7bb7
tommy3wood <thomaswcurran1014@gmail.com>
2020-01-17 10:56:54 -0600

    post & udpate

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -5,10 +5,35 @@ class Api::RecipesController < ApplicationController
     render 'index.json.jb'
   end
 
+  def create
+    @recipe = Recipe.new(
+                          title: params[:title], 
+                          chef: params[:chef],
+                          ingredients: params[:ingredients],
+                          directions: params[:directions],
+                          prep_time: params[:prep_time]
+                          )
+    @recipe.save
+    render 'show.json.jb' #It would be pointless to create a viewer for a post action. So instead you render a viewer that displays individual id's
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     render 'show.json.jb'
   end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.image_url = params[:image_url] || @recipe.image_url
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+
+    @recipe.save
+    render 'show.json.jb'
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/recipes' => 'recipes#index'
+    post '/recipes' => 'recipes#create'
     get '/recipes/:id' => 'recipes#show'
+    patch '/recipes/:id' => 'recipes#update'
   end
 end
